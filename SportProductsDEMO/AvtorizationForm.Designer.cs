5fcf9a51a851620b72aad00cc88a9627c8e9c95b
Gavrilovadasha <gavrilovadasha239@gmail.com>
2024-11-24 21:25:41 +0300

    Add project files.

---
+++ SportProductsDEMO/AvtorizationForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace SportProductsDEMO
+{
+    partial class AvtorizationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AvtorizationForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.txtBoxLogin = new System.Windows.Forms.TextBox();
+            this.txtBoxPass = new System.Windows.Forms.TextBox();
+            this.btnEditAvtor = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(178, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Авторизация";
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblLogin.Location = new System.Drawing.Point(81, 189);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(67, 24);
+            this.lblLogin.TabIndex = 1;
+            this.lblLogin.Text = "Логин:";
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblPass.Location = new System.Drawing.Point(81, 249);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(77, 24);
+            this.lblPass.TabIndex = 2;
+            this.lblPass.Text = "Пароль:";
+            // 
+            // txtBoxLogin
+            // 
+            this.txtBoxLogin.Location = new System.Drawing.Point(207, 192);
+            this.txtBoxLogin.Name = "txtBoxLogin";
+            this.txtBoxLogin.Size = new System.Drawing.Size(216, 22);
+            this.txtBoxLogin.TabIndex = 3;
+            // 
+            // txtBoxPass
+            // 
+            this.txtBoxPass.Location = new System.Drawing.Point(207, 252);
+            this.txtBoxPass.Name = "txtBoxPass";
+            this.txtBoxPass.Size = new System.Drawing.Size(216, 22);
+            this.txtBoxPass.TabIndex = 4;
+            // 
+            // btnEditAvtor
+            // 
+            this.btnEditAvtor.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnEditAvtor.Location = new System.Drawing.Point(183, 315);
+            this.btnEditAvtor.Name = "btnEditAvtor";
+            this.btnEditAvtor.Size = new System.Drawing.Size(157, 45);
+            this.btnEditAvtor.TabIndex = 5;
+            this.btnEditAvtor.Text = "Войти";
+            this.btnEditAvtor.UseVisualStyleBackColor = false;
+            this.btnEditAvtor.Click += new System.EventHandler(this.btnEditAvtor_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(119, 384);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(277, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Перейти к товарам в роли гостя";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // AvtorizationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(489, 520);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnEditAvtor);
+            this.Controls.Add(this.txtBoxPass);
+            this.Controls.Add(this.txtBoxLogin);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AvtorizationForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Авторизация";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.TextBox txtBoxLogin;
+        private System.Windows.Forms.TextBox txtBoxPass;
+        private System.Windows.Forms.Button btnEditAvtor;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
